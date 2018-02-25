64ea215c24db8d2838270eb9b51dc8fa650aedd8
tal2105 <jbualuang21@gmail.com>
2018-02-25 22:31:01 +0700

    helloworld.java
    Basic code java.

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+/* This is your first java program
+   Program name Hello.java
+   It displays "Hello World!" to standard output 
+*/
+class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); // display string
+	}
+}
